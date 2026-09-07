31503676b2e4e0b6096cc17cb36912bffdf1afd6
Wesley Benica <wtbenica@gmail.com>
2026-09-07 16:31:35 -0700

    bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,8 +38,8 @@ android {
         applicationId = "dev.benica.corvidcontacts"
         minSdk = 28
         targetSdk = 37
-        versionCode = 4
-        versionName = "1.0.3"
+        versionCode = 5
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
